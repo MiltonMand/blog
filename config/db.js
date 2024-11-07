4cac501eca3aef63fa8fe3f0fb57ce070e09105d
Milton Mand <johnstak82@gmail.com>
2024-11-07 09:51:04 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,1 @@
-if(process.env.NODE_ENV == "production") {
-    module.exports = {mongoURI : "mongodb+srv://milton:123@clusterteste.sn2lk.mongodb.net/?retryWrites=true&w=majority&appName=Clusterteste"}
-}else {
-    module.exports = {mongoURI : "mongodb://localhost/blogapp"}
-}
+module.exports = { mongoURI: "mongodb+srv://milton:123@clusterteste.sn2lk.mongodb.net/?retryWrites=true&w=majority&appName=Clusterteste" }
